23cf9c7136b6c67cf0fb48e7b339155a232b94a1
Daniel Tapia <dtapias@unsa.edu.pe>
2021-11-29 18:57:06 -0500

    Proyecto preaparado para realizar conexion a mongodb

--- Proyecto/app/src/main/java/com/example/myapplication/Login.java
+++ Proyecto/app/src/main/java/com/example/myapplication/Login.java
@@ -6,6 +6,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import io.realm.Realm;
+import io.realm.mongodb.mongo.MongoClient;
+
 
 public class Login extends AppCompatActivity {
     Button registrarse;
