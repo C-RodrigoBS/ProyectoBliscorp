d15cca4d69b4dc59367085c56d8110eda3aba02a
Delia Peralta <deliarocio08@gmail.com>
2021-12-02 20:59:55 -0500

    Generador de contratos2

---
+++ Proyecto/app/src/main/java/com/example/myapplication/Calculadora_Impuestos.java
@@ -0,0 +1,14 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Calculadora_Impuestos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculadora_impuestos);
+    }
+}
