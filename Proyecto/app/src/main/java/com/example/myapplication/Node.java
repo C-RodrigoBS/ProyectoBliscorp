55a958b6a8e3cb29fc81558407ae179a388a4173
Daniel Tapia <dtapias@unsa.edu.pe>
2021-12-03 00:42:51 -0500

    Clase lru preparada

---
+++ Proyecto/app/src/main/java/com/example/myapplication/Node.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pruebas;
+
+class Node<T, U> {
+    Node<T, U> previous;
+    Node<T, U> next;
+    T key;
+    U value;
+
+    public Node(Node<T, U> previous, Node<T, U> next, T key, U value){
+        this.previous = previous;
+        this.next = next;
+        this.key = key;
+        this.value = value;
+    }
+}
